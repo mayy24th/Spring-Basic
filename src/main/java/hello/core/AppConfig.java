1866651aaf064bdf3830bd9c85b46c9b251fd97a
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-03-27 12:21:22 +0900

    MainDiscountPolicy 애노테이션 생성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -30,8 +30,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService(){
         System.out.println("Call AppConfig.orderService");
-//        return new OrderServiceImpl(memberRepository(), discountPolicy());
-        return null;
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
     public DiscountPolicy discountPolicy() {
